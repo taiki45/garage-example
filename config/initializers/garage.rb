bb3b0d2b4568e4466d8f19f97d09eb2f1122259d
Taiki ONO <taiki-ono@cookpad.com>
2014-10-30 16:52:28 +0900

    Readable and writable access for user resource

--- config/initializers/garage.rb
+++ config/initializers/garage.rb
@@ -1,5 +1,10 @@
 Garage.configure {}
-Garage::TokenScope.configure {}
+Garage::TokenScope.configure do
+  register :public, desc: 'acessing publicly available data' do
+    access :read, User
+    access :write, User
+  end
+end
 
 Doorkeeper.configure do
   orm :active_record
