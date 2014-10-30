98a62a75f376940bfd22336889e0c545ab7d7959
Taiki ONO <taiki-ono@cookpad.com>
2014-10-30 16:42:21 +0900

    Define user resource: step 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+  include Garage::Representer
+  include Garage::Authorizable
+
+  property :id
+  property :name
+  property :email
+
+  def self.build_permissions(perms, other, target)
+  end
+
+  def build_permissions(perms, other)
+  end
 end
