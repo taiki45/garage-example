a1111eb9e4e877fc2013477c655abdf88fbf4142
Taiki ONO <taiki-ono@cookpad.com>
2014-10-30 16:50:05 +0900

    Set user resource access control
    public to read and writable only to single resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,11 @@ class User < ActiveRecord::Base
   property :email
 
   def self.build_permissions(perms, other, target)
+    perms.permits! :read
   end
 
   def build_permissions(perms, other)
+    perms.permits! :read
+    perms.permits! :write
   end
 end
